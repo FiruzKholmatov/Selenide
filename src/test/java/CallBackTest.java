ddf2d1a8761b11d834ad317e8447be03463f63ac
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-16 15:42:33 +0300

    headless on

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -18,7 +18,7 @@ public class CallBackTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
 
     }
